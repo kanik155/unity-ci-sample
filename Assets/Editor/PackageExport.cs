bd07d06126765a4542ba9db8fd48dea95380d6c4
kanik155 <takekanik3@gmail.com>
2020-06-17 02:53:44 +0900

    add PackageExport.cs

---
+++ Assets/Editor/PackageExport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class PackageExport
+{
+    [MenuItem("Tools/Export Unitypackage")]
+    public static void Export()
+    {
+        var root = "Scripts/CISample";
+        var exportPath = "./CISample.unitypackage";
+
+        var path = Path.Combine(Application.dataPath, root);
+        var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+            .Where(x => Path.GetExtension(x) == ".cs")
+            .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
+            .ToArray();
+
+        UnityEngine.Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+
+        AssetDatabase.ExportPackage(
+            assets,
+            exportPath,
+            ExportPackageOptions.Default);
+
+        UnityEngine.Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
+    }
+}
